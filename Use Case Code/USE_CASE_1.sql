5d67db224b189ffafd22e48f0f0ad2f6d137afc2
jasii <josephsepe@gmail.com>
2021-03-08 11:07:42 -0800

    Added use case 1

---
+++ Use Case Code/USE_CASE_1.sql
@@ -0,0 +1,13 @@
+/* Ruslan Kalashnikov & Joseph Sepe - CS276 WINTER 2021
+A Visitor makes a reservation for Feb 21, 2021 to Feb 27, 2021 for one of the Guest Houses, and a monk must be assigned for each of those weeks.
+Using one query, display the Visitor name, the Guest house, and the monk assigned for each week.
+*/
+
+select  visitor.first_name as "visitor first name", visitor.last_name as "visitor last name", check_in_date, check_out_date, 
+        time_card.fk_house_id as "house id", monk.first_name as "monk first name", time_card.start_date_time from visitor
+    join reservation on visitor_id = fk_visitor_id
+    join res_line_item on reservation_id = fk_reservation_id
+    join room on fk_room_id = room_id
+    join time_card on time_card.fk_house_id = room.fk_house_id
+    join monk on monk.monk_id = time_card.fk_monk_id
+        where time_card.start_date_time between check_in_date and check_out_date;
